c1b1015b567d126f905b2c4aeb88684b9a8b4cfa
@Egsagon <egsagon12@gmail.com>
2023-03-11 15:09:30 +0100

    removed threads

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from core import fetcher
+import os
+from time import sleep
+from core import fetcher, progress
 
-url = 'https://neko-sama.fr/anime/info/6604-shingeki-no-kyojin_vostfr'
-out = './result/'
+url = 'https://neko-sama.fr/anime/info/9520-tensei-shitara-slime-datta-ken_vostfr'
+out = './result/sdkfr_1/'
+
+# Create dirs
+if not os.path.exists(out): os.makedirs(out)
+
+cla = '[ CTHREAD ]'
+print(cla, 'Starting')
 
 syn, eps = fetcher.get_anime(url)
+open(out + 'syn.txt', 'w').write(syn)
 
-episode = eps[0]
-name = '_'.join(episode.split('/episode/')[1].split('-')[1:])
+print(cla, 'Wrote syn')
 
-print(f'{name = }')
+for episode in progress.Bar(cla, eps):
+    
+    name = '_'.join(episode.split('/episode/')[1].split('-')[1:])
+    
+    print(cla, '### Fething', name)
+    
+    prov, eurl = fetcher.get_episode(episode)
+    links = fetcher.get_episode_links(eurl)
+    
+    path = fetcher.download_episode(links, out + name + '.mp4')
+    
+    print(cla, f'### Fetched {name} ({path = })')
 
-prov, eurl = fetcher.get_episode(episode)
+print(cla, 'Finished process')
 
-links = fetcher.get_episode_links(eurl)
-
-# path = fetcher.download_episode(links, out + name + '.mp4', thread = False)
-
-path = fetcher.download_ffmpeg(links, out + name + '.mp4',
-                               thread = False)
-
-# BUG thread provide corrupted file
+# NOTE threads raises 429
